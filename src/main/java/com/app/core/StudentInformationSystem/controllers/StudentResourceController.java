38ab303799f0377553069758f3a3e3cd3784ae41
ankur5984 <ankur5984@gmail.com>
2022-01-24 18:33:48 +0530

    added slf4j configuration and captured on server.log.

--- src/main/java/com/app/core/StudentInformationSystem/controllers/StudentResourceController.java
+++ src/main/java/com/app/core/StudentInformationSystem/controllers/StudentResourceController.java
@@ -3,6 +3,8 @@ package com.app.core.StudentInformationSystem.controllers;
 import com.app.core.StudentInformationSystem.model.Student;
 import com.app.core.StudentInformationSystem.model.User;
 import com.app.core.StudentInformationSystem.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,8 @@ import java.util.List;
 @RequestMapping("/do.api.student")
 public class StudentResourceController {
 
+    Logger logger = LoggerFactory.getLogger(StudentResourceController.class);
+
     private final StudentService _service;
 
     public StudentResourceController(StudentService _service) {
@@ -22,9 +26,11 @@ public class StudentResourceController {
 
     @GetMapping("/all")
     public ResponseEntity<?> fetchAll(){
+
         List<Student> _students = _service.findAllStudent();
 
         if(_students!=null){
+            logger.info("Students List Controller {} : {} class",_students,StudentResourceController.class);
             return new ResponseEntity<>(_students, HttpStatus.OK);
         }
         return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
@@ -34,8 +40,10 @@ public class StudentResourceController {
     public ResponseEntity<?> getStudent(@RequestBody User _user) {
         Student _student = _service.getStudentDetails(_user.getUsername(), _user.getPassword());
         if (_student == null) {
+            logger.error("no student found");
             return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
         }
+        logger.info("Students found {}: {} class",_student, StudentResourceController.class);
         return new ResponseEntity<>(_student, HttpStatus.OK);
     }
 
@@ -45,12 +53,14 @@ public class StudentResourceController {
         if (_student == null) {
             return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
         }
+        logger.debug("Students found {} : {} class",_student, StudentResourceController.class);
         return new ResponseEntity<>(_student, HttpStatus.OK);
     }
 
     @GetMapping("/fetchById/{id}")
     public ResponseEntity<?> fetchById(@PathVariable("id") Long id){
         Student student = _service.getStudentById(id);
+        logger.info("Students found {} for id {} : {} class ",student,id, StudentResourceController.class);
         return new ResponseEntity<>(student,HttpStatus.OK);
     }
 
@@ -58,6 +68,7 @@ public class StudentResourceController {
     public ResponseEntity<?> registerEntry(@RequestBody Student _student){
         if(_student.getPassword().equals(_student.getConfirmPassword())) {
             Student newStudent = _service.addStudent(_student);
+            logger.info("Students found {}",newStudent);
             return new ResponseEntity<>(newStudent, HttpStatus.CREATED);
         }
         return new ResponseEntity<>("Password mismatch",HttpStatus.UNAUTHORIZED);
--- src/main/java/com/app/core/StudentInformationSystem/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/app/core/StudentInformationSystem/exceptionHandler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.app.core.StudentInformationSystem.exceptionHandler;
 
-import com.app.core.StudentInformationSystem.utils.AppLog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,20 +15,22 @@ import java.util.Date;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public final ResponseEntity<Object> handleAllExceptions(Exception _ex, WebRequest _request) {
-        AppLog.writeLog("BAD REQUEST","error", _ex.getMessage());
-        return buildErrorResponse(_ex,"BAD REQUEST "+_ex.getMessage(),HttpStatus.BAD_REQUEST,_request);
+     //   AppLog.writeLog("BAD REQUEST","error", _ex.getMessage());
+        logger.error("Error occurred at class {} with {} HTTP status with error message {}",GlobalExceptionHandler.class,HttpStatus.INTERNAL_SERVER_ERROR,_ex.getMessage());
+        return buildErrorResponse(_ex,"BAD REQUEST "+_ex.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR,_request);
     }
 
     @ExceptionHandler(StudentNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public final ResponseEntity<Object> handleUserNotFoundException(StudentNotFoundException ex, WebRequest request) {
-        AppLog.writeLog("NOT FOUND","error", ex.getMessage());
-        return buildErrorResponse(ex,HttpStatus.NOT_FOUND,request);
+    public final ResponseEntity<Object> handleUserNotFoundException(StudentNotFoundException _ex, WebRequest _request) {
+       // AppLog.writeLog("NOT FOUND","error", ex.getMessage());
+        logger.error("Error occurred at class {} with {} HTTP status with error message {}",GlobalExceptionHandler.class,HttpStatus.BAD_REQUEST,_ex.getMessage());
+        return buildErrorResponse(_ex,HttpStatus.NOT_FOUND,_request);
     }
 
     private ResponseEntity<Object> buildErrorResponse(Exception exception,
